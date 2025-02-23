6f277124a1cfbc0be4808ce19b42e378cef53daf
Rahul Jangid <rahuljangid0408@gmail.com>
2025-02-23 18:06:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const heroText = document.querySelector(".hero h1");
+    heroText.style.opacity = "0";
+    heroText.style.transition = "opacity 2s";
+    
+    setTimeout(() => {
+        heroText.style.opacity = "1";
+    }, 500);
+});
